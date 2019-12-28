360a5bd0ff750b7e83238436ff892ad536c9cd1c
supakorn <supakorn.wo@ku.th>
2019-12-28 13:28:19 +0700

    add tradelist page

--- src/pages/Content/Articles.js
+++ src/pages/Content/Articles.js
@@ -23,19 +23,22 @@ class Articles extends React.Component {
                         subTitle={"สายแฟตัวจริงต้องไม่พลาด! อัพเดทเทรน์แฟชั่นสุดปังปี 2020 สไตล์ไหนอิน ชิ้นไหนเอาท์ "}
                         description={"descriptiontetetetetete"}
                         imgLink={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg"}
-                    /><Card
+                    />
+                    <Card
                         category={"Fasion"}
                         title={"อัพเดทเทรน์แฟชั่นสุดปังปี 2020"}
                         subTitle={"สายแฟตัวจริงต้องไม่พลาด! อัพเดทเทรน์แฟชั่นสุดปังปี 2020 สไตล์ไหนอิน ชิ้นไหนเอาท์ "}
                         description={"descriptiontetetetetete"}
                         imgLink={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg"}
-                    /><Card
+                    />
+                    <Card
                         category={"Fasion"}
                         title={"อัพเดทเทรน์แฟชั่นสุดปังปี 2020"}
                         subTitle={"สายแฟตัวจริงต้องไม่พลาด! อัพเดทเทรน์แฟชั่นสุดปังปี 2020 สไตล์ไหนอิน ชิ้นไหนเอาท์ "}
                         description={"descriptiontetetetetete"}
                         imgLink={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg"}
-                    /><Card
+                    />
+                    <Card
                         category={"Fasion"}
                         title={"อัพเดทเทรน์แฟชั่นสุดปังปี 2020"}
                         subTitle={"สายแฟตัวจริงต้องไม่พลาด! อัพเดทเทรน์แฟชั่นสุดปังปี 2020 สไตล์ไหนอิน ชิ้นไหนเอาท์ "}
