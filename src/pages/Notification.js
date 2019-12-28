76c71be1241547f6f37446a52656c21e708d6c08
Ming <mingbackmountain@gmail.com>
2019-12-28 15:23:28 +0700

    add notification

--- src/pages/Notification.js
+++ src/pages/Notification.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import './Notification.scss';
+import Footer from '../components/Footer'
 
 class RegisterForm extends Component {
     constructor(props) {
@@ -9,7 +10,14 @@ class RegisterForm extends Component {
     render() {
         return (
             <div id="notification">
-               <h1>Noti</h1>
+                <div className="container"> 
+                    <h1>ยินดีด้วยให้จัดส่งสินค้าไปที่</h1>
+                    <h3> 62/5 ถ.ลาดพร้าว แขวงจอมพล เขตจุจักร กรุงเทพมหานคร 10900</h3>
+                    <button className="btn btn-success">ส่งสินค้าแล้ว</button>
+                    <button className="btn  btn-secondary disabled">ได้รับสินค้า</button>
+                </div>
+               
+              <Footer/>
             </div>
         )
     }
