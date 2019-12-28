a764709082889711918a69bd14bf585e977f80fb
Ming <mingbackmountain@gmail.com>
2019-12-28 17:55:06 +0700

    change noti2

--- src/pages/Notification2.js
+++ src/pages/Notification2.js
@@ -13,20 +13,23 @@ class Notification2 extends Component {
 
     render() {
         return (
-            <div id="notification">
+            <div className = "notification" id="notification">
                 <div className="container"> 
                     <h1>ยินดีด้วย การแลกเปลี่ยนสำเร็จ</h1>
-                    <h3>คุณสามารถทำการจัดส่งสินค้าไปที่ ให้จัดส่งสินค้าไปที่</h3>
-                    <h3> 62/5 ถ.ลาดพร้าว แขวงจอมพล เขตจุจักร กรุงเทพมหานคร 10900</h3>
+                    <h3>คุณสามารถทำการจัดส่งสินค้าไปที่</h3>
+                    <h3 className = "location"> 62/5 ถ.ลาดพร้าว แขวงจอมพล เขตจุจักร กรุงเทพมหานคร 10900</h3>
+
+                    <p>เมื่อส่งเรียบร้อย ให้กดส่งสินค้าแล้ว และรอรับสินค้าที่ทำการแลกเปลี่ยน</p>
+                    <div className = "button-list">
                     <button className="btn  btn-secondary disabled" onClick={this.acceptProduct}>ส่งสินค้าแล้ว</button>
-                    <a href="point"><button className="btn btn-success">ได้รับสินค้า</button></a>
+                    <a href="/point"><button className="btn btn-success">ได้รับสินค้า</button></a>
+                    </div>
                 </div>
                
               <Footer/>
             </div>
         )
     }
-
 }
 
 export default Notification2
