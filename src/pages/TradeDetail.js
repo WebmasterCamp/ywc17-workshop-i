6dd396832875b08095d0cea2bb86e68f85700f32
mewiipk <mewphattanun@gmail.com>
2019-12-28 17:38:51 +0700

    update

--- src/pages/TradeDetail.js
+++ src/pages/TradeDetail.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
 import './TradeDetail.scss'
 import ProductCard from '../components/ProductCard'
-=======
-import './TradeList.scss'
->>>>>>> 3dd6e3caab2c9b035c7e12e25e7957f0ad08913a
 import HeaderLoggedIn from '../components/HeaderLoggedIn'
 import Footer from '../components/Footer'
 
@@ -17,7 +13,6 @@ class TradeDetail extends Component {
         return (
             <div id="tradeDetail">
                 <HeaderLoggedIn />
-<<<<<<< HEAD
                 <div className="product">
                     <div className = "item-img">
                         <img className ="item" src="http://code.slicecrowd.com/labs/4/images/t-shirt.png" />
@@ -25,41 +20,24 @@ class TradeDetail extends Component {
 
                     <div className='description'>
                         <p className="productTitle"> เสื้อยืดสกรีนลายภาษาอังกฤษ </p>
-                        <p className="productRank"> Rank: A </p>
-                        <p className="productWarranty"> Warranty: 5 Years</p>
-                        <a href="/selecttotrade"><button>สนใจ Trade</button></a>
-                    <button>Chat</button>
-                    </div>
-                </div>
-                <div className='owner'>
-                    <img src="" />
-                    <p>Supakorn</p>
-                    <p>Score *****</p>
-                </div>
-                <div className='Comment'>
-                    <p>Comment Review</p>
-=======
-                <div className="row container">
-                    <div className="col-6">
-                            <img src="http://code.slicecrowd.com/labs/4/images/t-shirt.png" />
-                            <div className='description'>
-                                        <p className="productTitle">
-                                            Title: test
-                                </p>
-                                        <p className="productRank">
-                                            Rank: A
-                                </p>
-                                        <p className="productWarranty">
-                                            Warranty: 5 Years
-                                </p>
+                        <p className="productRank"> คุณภาพ: A </p>
+                        <p className="productRank"> รับประกันสูงสุด: 800 บาท</p>
+                        <div className = "button-choose">
+                            <a href="/selecttotrade"><button>สนใจแลกเปลี่ยน</button></a>
+                            <button>พูดคุยกับผู้แลกเปลี่ยน</button>
+                        </div>
+
+                        <div className='owner'>
+                            <p className = "owner-detail">เจ้าของเสื้อผ้า</p>
+                            <img src="" />
+                            <p className = "productRank">Supakorn</p>
+                            <p className = "productRank">Score *****</p>
                         </div>
                     </div>
-
-                    <div className="col-6">
-
-                    </div>
->>>>>>> 3dd6e3caab2c9b035c7e12e25e7957f0ad08913a
                 </div>
+                {/* <div className='Comment'>
+                    <p>Comment Review</p>
+                </div> */}
 
                 <Footer />
             </div>
