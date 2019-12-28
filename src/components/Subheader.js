ff7fa27a2e23827a4f2b5cea6cf898a0f0f7c30f
Ming <mingbackmountain@gmail.com>
2019-12-28 15:07:56 +0700

    add background for subheader

--- src/components/Subheader.js
+++ src/components/Subheader.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './Subheader.scss'
 
 class Subheader extends React.Component {
     constructor(props) {
@@ -7,7 +8,7 @@ class Subheader extends React.Component {
     render() {
         return (
             <div id="subheader">
-                <img src="./logo.svg"/>
+                <img src="./logo.svg" width="300"/>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
             </div>
         )
