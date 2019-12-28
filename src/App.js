a3e04076ad665a1cc59eb485308c7be1691e24e7
supakorn <supakorn.wo@ku.th>
2019-12-28 09:24:18 +0700

    del unuse

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Home from './components/Home';
-import Login from './components/Login';
-import Todos from './components/Todos';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Todos from './pages/Todos';
 import {
   BrowserRouter as Router,
   Switch,
@@ -33,50 +33,3 @@ export default function App() {
     </Router>
   );
 }
-
-// function Home() {
-//   return <h2>Home</h2>;
-// }
-
-// function About() {
-//   return <h2>About</h2>;
-// }
-
-// function Topics() {
-//   let match = useRouteMatch();
-
-//   return (
-//     <div>
-//       <h2>Topics</h2>
-
-//       <ul>
-//         <li>
-//           <Link to={`${match.url}/components`}>Components</Link>
-//         </li>
-//         <li>
-//           <Link to={`${match.url}/props-v-state`}>
-//             Props v. State
-//           </Link>
-//         </li>
-//       </ul>
-
-//       {/* The Topics page has its own <Switch> with more routes
-//           that build on the /topics URL path. You can think of the
-//           2nd <Route> here as an "index" page for all topics, or
-//           the page that is shown when no topic is selected */}
-//       <Switch>
-//         <Route path={`${match.path}/:topicId`}>
-//           <Topic />
-//         </Route>
-//         <Route path={match.path}>
-//           <h3>Please select a topic.</h3>
-//         </Route>
-//       </Switch>
-//     </div>
-//   );
-// }
-
-// function Topic() {
-//   let { topicId } = useParams();
-//   return <h3>Requested topic ID: {topicId}</h3>;
-// }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,6 @@
 import React from "react";
 import logo from '../static/logo192.png'
-import { connect } from 'react-redux'
 import firebase from '../firebase';
-import { sessionAction } from '../actions/sessionAction'
 
 class Home extends React.Component {
     constructor(props) {
@@ -12,32 +10,21 @@ class Home extends React.Component {
         }
     }
     componentDidMount() {
-        firebase.auth().onAuthStateChanged(user => {
-            if (user) {
-                this.props.sessionAction(user.email)
-            }
-        })
+        // firebase.auth().onAuthStateChanged(user => {
+        //     if (user) {
+        //         this.props.sessionAction(user.email)
+                
+        //     }
+        // })
     }
     render() {
         return (
             <div>
-                <img src={logo} alt="Logo" />
-                {
-                    !!!this.props.currentUser ?
-                        <h1>Welcome To Todos Please Login</h1>
-                        :
-                        <h1>Welcome Back {this.props.currentUser}</h1>
-                }
+                <h1>Hello</h1>
             </div>
 
         )
     }
 }
 
-const mapStateToProps = state => ({
-    currentUser: state.sessionReducer.currentUser
-})
-const mapDispatchToProps = dispatch => ({
-    sessionAction: (user) => dispatch(sessionAction(user))
-})
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default Home;
