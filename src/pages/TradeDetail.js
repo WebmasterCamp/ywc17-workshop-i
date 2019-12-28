5d2d23ad8caeb19f6f70ea4f1d0db6cc1dca0c7c
Ming <mingbackmountain@gmail.com>
2019-12-28 16:52:08 +0700

    add ads

--- src/pages/TradeDetail.js
+++ src/pages/TradeDetail.js
@@ -11,7 +11,28 @@ class TradeDetail extends Component {
         return (
             <div id="tradeDetail">
                 <HeaderLoggedIn />
-                <div className="product">
+                <div className="row container">
+                    <div className="col-6">
+                            <img src="http://code.slicecrowd.com/labs/4/images/t-shirt.png" />
+                            <div className='description'>
+                                        <p className="productTitle">
+                                            Title: test
+                                </p>
+                                        <p className="productRank">
+                                            Rank: A
+                                </p>
+                                        <p className="productWarranty">
+                                            Warranty: 5 Years
+                                </p>
+                        </div>
+                    </div>
+
+                    <div className="col-6">
+
+                    </div>
+                </div>
+
+                {/* <div className="product">
                     <img src="http://code.slicecrowd.com/labs/4/images/t-shirt.png" />
                     <div className='description'>
                         <p className="productTitle">
@@ -34,7 +55,7 @@ class TradeDetail extends Component {
                 </div>
                 <div className='Comment'>
                     <p>Comment Review</p>
-                </div>
+                </div> */}
             </div>
         )
     }
