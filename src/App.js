f0d887cb1b683153d73db5ea30dd793a35a84d17
supakorn <supakorn.wo@ku.th>
2019-12-28 09:33:10 +0700

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import React from "react";
 import Home from './pages/Home';
-import Login from './pages/Login';
-import Todos from './pages/Todos';
+// import Login from './pages/Login';
+// import Todos from './pages/Todos';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
+  // Link,
 } from "react-router-dom";
 import './App.scss'
 export default function App() {
   return (
     <Router>
       <div>
-        <h1 className="title">React-Router-Redux-Firebase-Axios-Sass-Docker-Travis</h1>
-            <Link to="/">Home</Link>{'\u00A0'}
-            <Link to="/login">Login</Link>{'\u00A0'}
-            <Link to="/todos">Todos</Link>{'\u00A0'}
+        {/* <h1 className="title">React-Router-Redux-Firebase-Axios-Sass-Docker-Travis</h1> */}
+            {/* <Link to="/">Home</Link>{'\u00A0'} */}
+            {/* <Link to="/login">Login</Link>{'\u00A0'}
+            <Link to="/todos">Todos</Link>{'\u00A0'} */}
 
         <Switch>
-          <Route path="/login">
+          {/* <Route path="/login">
             <Login />
           </Route>
           <Route path="/todos">
             <Todos />
-          </Route>
+          </Route> */}
           <Route path="/">
             <Home />
           </Route>
--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
+  
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// Required for Redux store setup
-import { Provider } from 'react-redux'
-import configureStore from './store';
-
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <Provider store={configureStore()}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
