91385ffea6ac74fe3094e6c03ca9f9a29466e393
mewiipk <mewphattanun@gmail.com>
2019-12-28 17:51:34 +0700

    update

--- src/pages/TradeDetail.js
+++ src/pages/TradeDetail.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 import './TradeDetail.scss'
-<<<<<<< HEAD
 import ProductCard from '../components/ProductCard'
-=======
-import './TradeList.scss'
->>>>>>> 286145c5c0c1554857efbded243f7a455aab4d11
 import HeaderLoggedIn from '../components/HeaderLoggedIn'
 import Footer from '../components/Footer'
 
@@ -17,7 +13,6 @@ class TradeDetail extends Component {
         return (
             <div id="tradeDetail">
                 <HeaderLoggedIn />
-<<<<<<< HEAD
                 <div className="product">
                     <div className = "item-img">
                         <img className ="item" src="http://code.slicecrowd.com/labs/4/images/t-shirt.png" />
@@ -30,21 +25,6 @@ class TradeDetail extends Component {
                         <div className = "button-choose">
                             <a href="/selecttotrade"><button>สนใจแลกเปลี่ยน</button></a>
                             <button>พูดคุยกับผู้แลกเปลี่ยน</button>
-=======
-                <div className="row container">
-                    <div className="col-6">
-                            <img src="http://code.slicecrowd.com/labs/4/images/t-shirt.png" />
-                            <div className='description'>
-                                        <p className="productTitle">
-                                            Title: test
-                                </p>
-                                        <p className="productRank">
-                                            Rank: A
-                                </p>
-                                        <p className="productWarranty">
-                                            Warranty: 5 Years
-                                </p>
->>>>>>> 286145c5c0c1554857efbded243f7a455aab4d11
                         </div>
 
                         <div className='owner'>
