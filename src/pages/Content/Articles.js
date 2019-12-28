78fe18a36231453eb4c47807e780b3df12048bef
mewiipk <mewphattanun@gmail.com>
2019-12-28 17:03:44 +0700

    update

--- src/pages/Content/Articles.js
+++ src/pages/Content/Articles.js
@@ -18,7 +18,7 @@ class Articles extends React.Component {
                 <h1>บทความล่าสุด</h1>
                 <div className="cards">
                         <Card
-                            category={"Fasion"}
+                            category={"Fashion"}
                             title={"กระแส Eco-Fashion ผ่านมาแล้ว โปรดอย่าผ่านไป"}
                             subTitle={""}
                             description={""}
@@ -26,21 +26,21 @@ class Articles extends React.Component {
                             href={'/content1'}
                         />
                         <Card
-                            category={"Fasion"}
+                            category={"Fashion"}
                             title={"สายแฟตัวจริงต้องไม่พลาด! อัพเดทเทรน์แฟชั่นสุดปังปี 2020 สไตล์ไหนอิน ชิ้นไหนเอาท์"}
                             description={""}
                             imgLink={"/content-logo-2.jpg"}
                             href={'/content2'}
                         />
                         <Card
-                            category={"Fasion"}
+                            category={"Fashion"}
                             title={"สายแฟตัวจริงต้องไม่พลาด! อัพเดทเทรน์แฟชั่นสุดปังปี 2020 สไตล์ไหนอิน ชิ้นไหนเอาท์"}
                             description={""}
                             imgLink={"/content-logo-3.jpg"}
                             href={'/content3'}
                         />
                         <Card
-                            category={"Fasion"}
+                            category={"Fashion"}
                             title={"สายแฟตัวจริงต้องไม่พลาด! อัพเดทเทรน์แฟชั่นสุดปังปี 2020 สไตล์ไหนอิน ชิ้นไหนเอาท์"}
                             description={""}
                             imgLink={"/content-logo-4.jpg"}
--- src/pages/TradeList.js
+++ src/pages/TradeList.js
@@ -40,8 +40,8 @@ class TradeList extends Component {
                     <ProductCard />
                 </div>
                 <div className="page">
-                    <button>ย้อนกลับ</button>
-                    <button>ถัดไป</button>
+                    <button className ="btn">ย้อนกลับ</button>
+                    <button className ="btn">ถัดไป</button>
                 </div>
 
             </div>
